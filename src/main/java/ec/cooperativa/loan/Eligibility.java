4e6f30e4a38a02f4d6f8f6298a0e0740a15ce936
Issac-Maza <issamaza@espol.edu.ec>
2026-05-27 20:13:27 -0500

    checkstyle(HideUtilityClassConstructor): add private constructor to utility class

--- src/main/java/ec/cooperativa/loan/Eligibility.java
+++ src/main/java/ec/cooperativa/loan/Eligibility.java
@@ -23,6 +23,10 @@ public class Eligibility {
     public static List history = new ArrayList();
     public static int auditCounter = 0;
 
+    private Eligibility() {
+
+    }
+
     public static Map evaluate(Double income, Double debt, Integer tenureMonths, Integer age, Double savingsBalance, Integer latePayments, Integer dependents, boolean isEmployee, boolean isPensioner, boolean hasGuarantor, String statusTag) {
 
         Map entry = new HashMap();
